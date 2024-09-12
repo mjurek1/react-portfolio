678a9bb79a9a7d144d6fbcccda95eca9f77e30d6
mjurek1 <jurekmichael0@gmail.com>
2024-09-11 23:05:23 -0500

    set up initial routing to load appropriate views

--- react-portfolio-project/src/main.jsx
+++ react-portfolio-project/src/main.jsx
@@ -1,10 +1,52 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+import App from './App';
+import ErrorPage from './pages/ErrorPage';
+import AboutMePage from './pages/AboutMePage';
+import ContactPage from './pages/ContactPage';
+import PortfolioPage from './pages/PortfolioPage';
+import ResumePage from './pages/ResumePage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <AboutMePage />,
+      },
+      {
+        index: true,
+        element: <ContactPage />,
+      },
+      {
+        index: true,
+        element: <PortfolioPage />,
+      },
+      {
+        index: true,
+        element: <ResumePage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
