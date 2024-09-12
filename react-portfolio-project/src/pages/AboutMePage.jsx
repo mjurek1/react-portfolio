295f36d1cbe017c1adb3fb8160a861d4ed885404
mjurek1 <jurekmichael0@gmail.com>
2024-09-11 22:55:09 -0500

    setting up scaffolding

---
+++ react-portfolio-project/src/pages/AboutMePage.jsx
@@ -0,0 +1,9 @@
+export default function AboutMePage() {
+    return (
+        <div className="container pt-4">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit facere sit qui culpa obcaecati. Iure voluptate atque soluta aspernatur sed tenetur, a modi eum quisquam ut at eius quod neque.
+            </p>
+        </div>
+    )
+}
